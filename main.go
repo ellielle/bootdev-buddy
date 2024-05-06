15c26d6aac5e9b14ac90eb77111db3eec0be5b85
ellielle <40385743+ellielle@users.noreply.github.com>
2024-05-06 19:52:44 -0400

    add scripts to run and test on file change with watchexec

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 func main() {
 	// get the page to crawl from site variable "SITE"
 	site := os.Getenv("SITE")
-	getBDArchmages(site)
-	// getBDLeaderboard(env)
+
+	getDBArchmages(site)
+}
+
+func bootDevAPI(URL string) string {
+	api := make(map[string]string)
+	api["leaderboard"] = "https://api.boot.dev/v1/leaderboard_archmage"
+	return api[URL]
 }
