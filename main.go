6072fea49f3a6ee730abc8567bed6b8b328728f9
ellielle <40385743+ellielle@users.noreply.github.com>
2024-05-05 20:13:41 -0400

    add gocolly for scraping

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-func main() {
+import (
+	"os"
+)
 
-	getBDLeaderboard()
+func main() {
+	// get the page to crawl from site variable "SITE"
+	site := os.Getenv("SITE")
+	getBDArchmages(site)
+	// getBDLeaderboard(env)
 }
