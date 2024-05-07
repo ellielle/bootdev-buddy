e710e31839d453ded0179c26f5b204a0c8422d1d
ellielle <40385743+ellielle@users.noreply.github.com>
2024-05-07 17:12:35 -0400

    add basic crawling for Archmage leaderboard

--- bootdev.go
+++ bootdev.go
@@ -7,20 +7,28 @@ import (
 )
 
 func getDBArchmages(leaderboard string) {
-	// create a new request to https://api.boot.dev/v1/leaderboard_archmage
-	req, err := http.Get(leaderboard)
+	// Create a new request to https://api.boot.dev/v1/leaderboard_archmage
+	req, err := http.NewRequest("GET", leaderboard, nil)
 	if err != nil {
 		log.Fatalf("Something is wrong with the request URL, %v", err.Error())
 	}
 
+	// Send that request out!
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("Something went during the request: %v", err.Error())
+	}
+
+	// If the request succeeds, we'll marshal
+	// the JSON response into `archmages`
 	archmages := []Archmage{}
-	decoder := json.NewDecoder(req.Body)
+	decoder := json.NewDecoder(resp.Body)
 	err = decoder.Decode(&archmages)
 	if err != nil {
 		log.Fatalf("Error marshaling JSON data: %v", err.Error())
 	}
 
-	log.Println(decoder)
+	log.Println(archmages[0].Handle)
 
 	// FIXME: decoder has value, convert to json to see what you're working with
 }
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"log"
 	"os"
 )
 
@@ -8,14 +10,31 @@ func main() {
 	// get the page to crawl from site variable "SITE"
 	site := os.Getenv("SITE")
 
-	getDBArchmages(site)
+	// if the ENV variable "SITE" doesn't have a valid option,
+	// terminate the program
+	URL, err := bootDevAPI(site)
+	if err != nil {
+		log.Fatalf("Invalid API option\nValid options: archmage | stats | daily | karma\n")
+	}
+
+	getDBArchmages(URL)
 }
 
-func bootDevAPI(URL string) string {
+// bootDevAPI takes any of the strings below
+// as map keys, and returns the url for that endpoint
+// archmage = Archmage Leaderboard data
+// stats = Global daily stats
+// daily = Top daily learners - based on XP earned
+// karma: Discord community leaderboard - based on a bunch of Discordiness
+func bootDevAPI(URL string) (string, error) {
 	api := make(map[string]string)
 	api["archmage"] = "https://api.boot.dev/v1/leaderboard_archmage"
 	api["stats"] = "https://api.boot.dev/v1/leaderboard_stats"
 	api["daily"] = "https://api.boot.dev/v1/leaderboard_xp/day?limit=30"
 	api["karma"] = "https://api.boot.dev/v1/leaderboard_karma/alltime?limit=30"
-	return api[URL]
+	if _, ok := api[URL]; !ok {
+		return "", errors.New("Invalid API")
+
+	}
+	return api[URL], nil
 }
