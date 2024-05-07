84ba91716458996cbb00685fc9052956659b2683
ellielle <40385743+ellielle@users.noreply.github.com>
2024-05-07 11:54:17 -0400

    update with the rest of the endpoints + tests

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ func main() {
 
 func bootDevAPI(URL string) string {
 	api := make(map[string]string)
-	api["leaderboard"] = "https://api.boot.dev/v1/leaderboard_archmage"
+	api["archmage"] = "https://api.boot.dev/v1/leaderboard_archmage"
+	api["stats"] = "https://api.boot.dev/v1/leaderboard_stats"
+	api["daily"] = "https://api.boot.dev/v1/leaderboard_xp/day?limit=30"
+	api["karma"] = "https://api.boot.dev/v1/leaderboard_karma/alltime?limit=30"
 	return api[URL]
 }
--- main_test.go
+++ main_test.go
@@ -11,12 +11,14 @@ func TestMain(t *testing.T) {
 		input string
 		want  string
 	}{
-		"leaderboard": {input: "leaderboard", want: "https://api.boot.dev/v1/leaderboard_archmage"},
+		"archmage": {input: "archmage", want: "https://api.boot.dev/v1/leaderboard_archmage"},
+		"stats":    {input: "stats", want: "https://api.boot.dev/v1/leaderboard_stats"},
+		"daily":    {input: "daily", want: "https://api.boot.dev/v1/leaderboard_xp/day?limit=30"},
+		"karma":    {input: "karma", want: "https://api.boot.dev/v1/leaderboard_karma/alltime?limit=30"},
 	}
 
 	// Tests that the proper url is returned
 	// and that the function works itself
-	// TODO: expand as app grows
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
 			got := bootDevAPI(tc.input)
