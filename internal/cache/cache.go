ccefe3be0d8972d99794968ca26245011e5c0eff
ellielle <40385743+ellielle@users.noreply.github.com>
2024-05-07 21:54:59 -0400

    add basic internal cache with reaping
    get and add methods completed

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,3 +1,66 @@
 package cache
 
-// TODO: all of it
+import (
+	"sync"
+	"time"
+)
+
+// cacheEntry is expecting JSON as a []byte
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+// Cache includes a mutex because maps aren't
+// concurrency safe
+type Cache struct {
+	Entries map[string]cacheEntry
+	mu      *sync.RWMutex
+}
+
+func NewCache(interval time.Duration) Cache {
+	cache := Cache{
+		Entries: make(map[string]cacheEntry),
+		mu:      &sync.RWMutex{},
+	}
+	go cache.reapLoop(interval)
+
+	return cache
+}
+
+func (c *Cache) Add(key string, val *[]byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.Entries[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       *val,
+	}
+}
+
+func (c *Cache) Get(entry string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	data, ok := c.Entries[entry]
+	return data.val, ok
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(time.Now().UTC(), interval)
+
+	}
+}
+
+func (c *Cache) reap(now time.Time, interval time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for key, entry := range c.Entries {
+		if entry.createdAt.Before(now.Add(-interval)) {
+			delete(c.Entries, key)
+		}
+	}
+
+}
