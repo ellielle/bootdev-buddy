303f87fd1b23f13f1d2190fe592324cde14b84c7
ellielle <40385743+ellielle@users.noreply.github.com>
2024-05-08 09:45:37 -0400

    add error handling function to dry things a little

--- bootdev.go
+++ bootdev.go
@@ -4,31 +4,36 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 )
+
+func check(e error, message string) {
+	if e != nil {
+		log.Fatalf(message, e.Error())
+	}
+}
 
 func getDBArchmages(leaderboard string) {
 	// Create a new request to https://api.boot.dev/v1/leaderboard_archmage
 	req, err := http.NewRequest("GET", leaderboard, nil)
-	if err != nil {
-		log.Fatalf("Something is wrong with the request URL, %v", err.Error())
-	}
+	check(err, "Something is wrong with the request URL, %v")
 
 	// Send that request out!
 	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Fatalf("Something went during the request: %v", err.Error())
-	}
+	check(err, "Something went during the request: %v")
 
 	// If the request succeeds, we'll marshal
 	// the JSON response into `archmages`
 	archmages := []Archmage{}
 	decoder := json.NewDecoder(resp.Body)
 	err = decoder.Decode(&archmages)
-	if err != nil {
-		log.Fatalf("Error marshaling JSON data: %v", err.Error())
-	}
+	check(err, "Error marshaling JSON data: %v")
 
 	log.Println(archmages[0].Handle)
 
 	// FIXME: decoder has value, convert to json to see what you're working with
+	file, err := os.Create("archmages.json")
+	check(err, "Error creating archmages.json: %v")
+	defer file.Close()
+
 }
