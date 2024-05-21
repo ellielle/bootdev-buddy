c8582f813e7335fc84af19a991f69b230f23c552
ellielle <40385743+ellielle@users.noreply.github.com>
2024-05-21 19:41:59 -0400

    add login methods to auth with boot.dev

--- internal/login/login.go
+++ internal/login/login.go
@@ -7,7 +7,12 @@ import (
 	"net/http"
 )
 
-func ExchangeOTPForToken(OTP string) (string, error) {
+type LoginResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+func ExchangeOTPForToken(OTP string) (LoginResponse, error) {
 	// Boot.Dev's one-time password login for their CLI app
 	// The token returned can be used to authenticate the user
 	// across their API
@@ -17,42 +22,37 @@ func ExchangeOTPForToken(OTP string) (string, error) {
 		Otp string `json:"Otp"`
 	}
 
-	type loginResponse struct {
-		AccessToken  string `json:"access_token"`
-		RefreshToken string `json:"refresh_token"`
-	}
-
 	// populate the struct and then Marshal it into a []byte
 	// to be used in the HTTP request
 	apiReq := apiRequest{Otp: OTP}
 	jsonData, err := json.Marshal(apiReq)
 	if err != nil {
-		return "", err
+		return LoginResponse{}, err
 	}
 
 	// Create and carry out a new HTTP request to the API
 	req, err := http.NewRequest("POST", OTP_LOGIN, bytes.NewBuffer(jsonData))
 	if err != nil {
-		return "", err
+		return LoginResponse{}, err
 	}
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "", err
+		return LoginResponse{}, err
 	}
 	defer resp.Body.Close()
 
 	token, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return LoginResponse{}, err
 	}
 
-	loginResp := loginResponse{}
+	loginResp := LoginResponse{}
 	err = json.Unmarshal(token, &loginResp)
 	if err != nil {
-		return "", err
+		return LoginResponse{}, err
 	}
 
-	return loginResp.AccessToken, nil
+	return loginResp, nil
 }
