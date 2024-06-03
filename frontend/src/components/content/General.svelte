81aa07a552220a00af641b4ba6925bcc5a6a080f
ellielle <40385743+ellielle@users.noreply.github.com>
2024-06-03 16:38:53 -0400

    migrate to using svelte store

--- frontend/src/components/content/General.svelte
+++ frontend/src/components/content/General.svelte
@@ -1,4 +1,7 @@
 <script>
+  import { User } from "../../stores/user";
 </script>
 
-<main>Welcome to Boot.dev Buddy!</main>
+<main>
+  Welcome to Boot.dev Buddy, {$User.userData.DiscordUserHandle}!
+</main>
