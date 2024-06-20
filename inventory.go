25b95d62039ec9cc52a45f4bccf58b748509996b
ellielle <40385743+ellielle@users.noreply.github.com>
2024-06-20 13:44:37 -0400

    fix staticcheck errors

--- inventory.go
+++ inventory.go
@@ -19,10 +19,6 @@ const FLAME_URL = "https://api.boot.dev/v1/user_frozen_flame"
 
 // SeerStones gets the user's current amount of Seer Stones
 func (a *App) SeerStones() (int, error) {
-	type repsonse struct {
-		Stones int `json:"stones"`
-	}
-
 	req, err := http.NewRequest("GET", STONES_URL, nil)
 	if err != nil {
 		return 0, err
@@ -45,14 +41,9 @@ func (a *App) SeerStones() (int, error) {
 	num, err := strconv.Atoi(string(stones))
 
 	return num, err
-
 }
 
 func (a *App) BakedSalmon() (int, error) {
-	type response struct {
-		Salmon int `json:"salmon"`
-	}
-
 	req, err := http.NewRequest("GET", SALMON_URL, nil)
 	if err != nil {
 		return 0, err
@@ -123,5 +114,5 @@ func (a *App) FrozenFlames() (int, error) {
 	err = decoder.Decode(&frozenFlames)
 
 	// only need the length of the FrozenFlames slice
-	return len(frozenFlames), nil
+	return len(frozenFlames), err
 }
